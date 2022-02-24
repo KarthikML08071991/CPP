2d77d3a1d7ab77a24e98fd286f7b73130bd0d825
Karthik M L <94063813+KarthikML08071991@users.noreply.github.com>
2022-02-24 08:44:52 +0530

    Udemy Challenge1
    Beginning C++ Programming - From Beginner to Beyond Challenge1, Section: 4, Chapter 34.

---
+++ challenge1Udemy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a favorite number between 1 and 100: ";
+    int favoriteNumber = 0;
+    cin >> favoriteNumber;
+    cout << "Amazing!! That's my favorite number too!" << endl;
+    cout << "No really!!, " << favoriteNumber << " is my favorite number!" << endl;
+    return 0;
+}
