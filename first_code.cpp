1fffc98802dd4c07ab1427b993a04695fdfdd349
KarthikML08071991 <94063813+KarthikML08071991@users.noreply.github.com>
2022-02-24 06:46:30 +0530

    First C++ code, just basics, with a story.
    First C++ code with a story guided by C++ Tutorial for Beginners - Full Course from freecodecamp.org, on YouTube: https://www.youtube.com/watch?v=vLnPwxZdW4Y. This is to learn the Data Structures with Algorithms in MCA 1st Semester.

---
+++ first_code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Here is a short story about Jhon:" << endl;
+
+    string charcterNAme = "Jhon";
+    int characterAge = 40;
+
+    cout << "There was a man named " << charcterNAme << "." << endl;
+    cout << "He was " << characterAge << " years old." << endl;
+    cout << "He liked his name, " << charcterNAme << ".... But..." << endl;
+    cout << "He did not like being " << characterAge << " years old." << endl;
+
+    return 0;
+}
