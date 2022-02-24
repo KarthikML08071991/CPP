e10d8795cb63197f5ed166204a5cfe73b2a91927
Karthik M L <94063813+KarthikML08071991@users.noreply.github.com>
2022-02-24 08:28:33 +0530

    Old style in/out without namespace std
    Old C++ code style without using namespace std and using std::cout and std::cin.

---
+++ old_style_in-out.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Enter your favorite number: ";
+    int favorite_number = 0;
+    std::cin >> favorite_number;
+    std::cout << "Amazing! That's my favorite number too!" << std::endl;
+    return 0;
+}
